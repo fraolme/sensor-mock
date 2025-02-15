a241eddd15438686e233b2affbf6c6289166438e
Fraol <88104941+fraolme@users.noreply.github.com>
2025-02-15 17:07:46 +0300

    multithreaded mock sensor server and client

---
+++ sensor-client/Program.cs
@@ -0,0 +1,74 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+namespace Program {
+    class Program {
+        public static async Task Main(string[] args) {
+            var client = new SensorClient();
+            if (args.Length == 0) {
+                Console.WriteLine(
+                    @"USAGE: sensor-client command
+Available Commands: 
+    GET_STATUS
+    GET_TEMP
+    GET_PRESSURE
+    GET_RPM");
+                return;
+            }
+
+            await client.SendCommand(args[0]);
+        }
+    }
+
+    class SensorClient {
+
+        private readonly Logger logger;
+        public SensorClient() {
+            logger = new Logger();
+        }
+
+        public async Task SendCommand(string command) {
+            var host = "127.0.0.1";
+            var port = 5611;
+            logger.Log("Conecting to server...", LogLevel.Info);
+            try {
+                
+                using var server = new TcpClient(host, port);
+                using NetworkStream stream = server.GetStream();
+                logger.Log($"Sending Command: {command}", LogLevel.Info);
+                var bytes = Encoding.ASCII.GetBytes(command);
+                await stream.WriteAsync(bytes, 0, bytes.Length);
+                var buffer = new byte[1024];
+                var bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
+                var response = Encoding.ASCII.GetString(buffer.Take(bytesRead).ToArray());    
+
+                Console.WriteLine($"RESPONSE FROM SERVER: {response}");
+            } catch(SocketException) {
+                logger.Log($"Unable to Connect to server: Make sure the server is running at {host}:{port}", LogLevel.Error);
+            } catch(Exception e) {
+                logger.Log($"UNKNOWN ERROR: {e}", LogLevel.Error);
+            }
+        }
+    }
+
+    class Logger {
+        public Logger(){
+        }
+
+        public void Log(string message, LogLevel level) {    
+            switch(level) {
+                case LogLevel.Info:
+                    Console.WriteLine($"[INFO]: {message}");
+                    break;
+                case LogLevel.Error:
+                    Console.WriteLine($"[ERROR]: {message}");
+                    break;
+            }
+        }     
+    }
+
+    enum LogLevel {
+        Info,
+        Error,
+    } 
+}
---
+++ sensor-server/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace Program {
+    class Program {
+        public static async Task Main(string[] args) {
+            var server = new SensorServer();
+            await server.Start();
+        } 
+    }
+
+    class SensorServer {
+
+        private readonly Logger logger;
+
+        public SensorServer() {
+            logger = new Logger();
+        }
+
+        public async Task Start() {
+            var port = 5611;
+            logger.Log($"Server Started Listening on Port : {port}");
+
+            var listener = new TcpListener(IPAddress.Loopback, port);   
+            listener.Start();
+
+            Console.CancelKeyPress += delegate {
+                listener.Stop();
+                logger.Log($"Server Exiting");
+            };
+            
+            while(true) {
+                TcpClient client = await listener.AcceptTcpClientAsync();
+                Task.Run(async () => await HandleClient(client));
+            }
+        }
+    
+        async Task HandleClient(TcpClient client)
+        {
+            logger.Log($"Accepted New Connection", client.Client.RemoteEndPoint, LogLevel.Info);
+            try {
+                var buffer = new byte[1024];
+
+                using NetworkStream stream = client.GetStream();
+                var bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
+                var command = Encoding.ASCII.GetString(buffer.Take(bytesRead).ToArray());          
+
+                await HandleCommand(command.ToString(), stream, client.Client.RemoteEndPoint);
+            } catch(Exception ex) {
+                logger.Log(ex, client.Client.RemoteEndPoint);
+            } finally {
+                client.Close();
+            }
+        }
+
+        async Task HandleCommand(string command, NetworkStream stream, EndPoint endPoint) {
+            logger.Log($"Sending Response to Command : {command}",  endPoint, LogLevel.Info);
+            string response;
+            switch (command) {
+                case "GET_STATUS":
+                    response = "Active";                
+                    break;
+                case "GET_TEMP":
+                    response = "35.6C";
+                    break;
+                case "GET_PRESSURE":
+                    response = "450P";
+                    break;
+                case "GET_RPM":
+                    response = "341RPM";
+                    break;
+                default:
+                    response = $"UNKNOWN COMMAND: {command}";
+                    logger.Log(response ,endPoint, LogLevel.Error);
+                    break;
+            }
+
+            var bytes = Encoding.ASCII.GetBytes(response);
+            await stream.WriteAsync(bytes, 0, bytes.Length);
+        }
+    }
+
+    class Logger {
+        public Logger(){
+        }
+
+        public void Log(string message, EndPoint endPoint, LogLevel level) {    
+            switch(level) {
+                case LogLevel.Info:
+                    Console.WriteLine($"[INFO]: {endPoint}: {message}");
+                    break;
+                case LogLevel.Error:
+                    Console.WriteLine($"[ERROR]: {endPoint}: {message}");
+                    break;
+            }
+        }     
+
+        public void Log(Exception ex, EndPoint endPoint) {    
+            Log($"{ex.Message} \n {ex.StackTrace}", endPoint, LogLevel.Error);
+        }    
+
+        public void Log(string message) {
+            Console.WriteLine($"[INFO]: {message}");
+        }
+    }
+
+    enum LogLevel {
+        Info,
+        Error,
+    } 
+}
